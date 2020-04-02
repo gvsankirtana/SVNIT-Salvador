a64ad079dc1d20669dfffb88672b474f6c9fa45c
Pritesh Kumar Tripathi <pritesh.kt@gmail.com>
2020-04-02 18:42:11 +0530

    Server working
    Changes made:
    1. Added Nodemon which tracks every change and then refreshes browser at every change read more on google.
    2. Added script in package.json so just write npm start to start server.
    3. Update sql query.
    4. Updated the api end point to submit in script.js
    5. Added views folder for pug support.

---
+++ createDB.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS amoc;
+USE amoc;
+CREATE TABLE userinfo(
+    email varchar(255) NOT NULL, 
+    name varchar(255) NOT NULL,
+    admNo varchar(255),
+    year varchar(3),
+    division char(1),
+    password varchar(10),
+    PRIMARY KEY (email),
+    UNIQUE (admNo)
+);
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyparser=require('body-parser')
 const mysql = require('mysql');
+const path = require('path');
+const pug = require('pug');
 
 const app = express();
 app.use(express.static('../library'))
@@ -8,14 +10,13 @@ app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json());
 app.set('view engine','pug')
 
-
-
+app.use(express.static(__dirname + '/'));
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "pass",
-  database: "mysql"
+  password: "password",
+  database: "amoc"
 });
 
 
@@ -24,26 +25,28 @@ con.connect(function(err) {
   console.log("Connected!");
 
 });
+
 app.listen(3000,()=>console.log('express opened'));
 
 app.get('/', function (req,res) {
-   res.sendFile('login.html',{root:amoc});
+   res.sendFile(path.join(__dirname + '/login.html'));
 }); 
 
 
-app.post('/login.html', function(req, res){
+app.post('/signup', function(req, res){
   //if you use body-parser, the user submitted form data will
   //be inside req.body
   console.log("data:", req.body);
-var sql = "INSERT INTO userinfo VALUES ('"+req.body.email+"','"+req.body.name+"','"+req.body.adm+"','"+req.body.year"','"+req.body.div+"','"+req.body.pwd+"')";
+  let sql = `INSERT INTO userinfo (email, name, admNo, year, division, password) VALUES('${req.body.email}', '${req.body.name}', '${req.body.adm}', '${req.body.year}', '${req.body.div}', '${req.body.pwd}')`;
   con.query(sql, function (err, rows, fields) {
-    if (err) throw err;
+    if (err){
+      console.log(err);
+    };
     console.log("1 record inserted");
-    console.log("the solution is:",rows[0].solution)
+    // console.log("the solution is:",rows[0].solution)
   });
 
-  res.send(html);
+  // res.send(req.body.email);
   res.render('index',{title:'data saved successfully',message:'data saved'})
 });
  
-con.end();
